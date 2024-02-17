e2ec35de1b164836d84411c5de1e4e0d910a6037
ramanboff <bayramofframan0@gmail.com>
2024-02-17 17:03:47 +0400

    Output statement added

--- Tip Calculator/src/components/Output.jsx
+++ Tip Calculator/src/components/Output.jsx
@@ -1,12 +1,12 @@
 
 
-const Output = ({billInput,setBillInput,yourProsentage, friendsProsentage, }) => {
+const Output = ({billInput, tip, commonBill}) => {
 
 
 
   return (
     <div>
-      <span>`You Pay  (${billInput} + $12 tip)</span>
+      <span><b>You Pay ${commonBill}  (${billInput} + ${tip} tip)</b></span>
     </div>
   )
 }
