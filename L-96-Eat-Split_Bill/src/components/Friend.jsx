77fec0a5d71c4b90bee3d48ae5c5641867c51518
ramanboff <bayramofframan0@gmail.com>
2024-02-18 00:21:51 +0400

    Added Select button in Friend component

--- L-96-Eat-Split_Bill/src/components/Friend.jsx
+++ L-96-Eat-Split_Bill/src/components/Friend.jsx
@@ -1,15 +1,23 @@
-
-const Friends = ({friend}) => {
+const Friends = ({ friend }) => {
   return (
     <li>
       <img src={friend.image} />
       <h3>{friend.name}</h3>
-  {friend.balance <0 && <p className="red">You owe {friend.name}{friend.balance}$</p>}
-  {friend.balance >0 && <p className="green">{friend.name} owes you {friend.balance}$</p>}
-  {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-  
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name}
+          {friend.balance}$
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {friend.balance}$
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <button className="button">Select</button>
     </li>
-  )
-}
+  );
+};
 
-export default Friends
+export default Friends;
