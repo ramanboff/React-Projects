d3a274ffe5eb3d2c967ea609a4558a6c9c4a3b2a
ramanboff <bayramofframan0@gmail.com>
2024-02-17 17:04:18 +0400

    code formatting

--- Tip Calculator/src/components/SelectProcentage.jsx
+++ Tip Calculator/src/components/SelectProcentage.jsx
@@ -1,23 +1,18 @@
-
-
-
-const SelectProcentage = ({ prosentage, setProsentage, children}) => {
-
-
-
-
-
+const SelectProcentage = ({ prosentage, setProsentage, children }) => {
   return (
     <div>
       <label>{children}</label>
-      <select value={prosentage} onChange={(e) => setProsentage(e.target.value)}>
+      <select
+        value={prosentage}
+        onChange={(e) => setProsentage(Number(e.target.value))}
+      >
         <option value="0">Dissatisfied (0%)</option>
         <option value="5">It was Ok (5%)</option>
         <option value="10">It was good (10%)</option>
         <option value="20">Absolutely amazing! (20%)</option>
       </select>
     </div>
-  )
-}
+  );
+};
 
-export default SelectProcentage
+export default SelectProcentage;
