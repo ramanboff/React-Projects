0cf0e7591a6780d279a8725a0556a43806c96a43
ramanboff <bayramofframan0@gmail.com>
2024-02-17 12:55:18 +0400

    billInput state created

--- Tip Calculator/src/components/BillInput.jsx
+++ Tip Calculator/src/components/BillInput.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
+
 const Bill = () => {
 
-
+const [billInput, setBillInput] = useState(0)
 
   return <div>
    <span>How mutch was the bill?</span>
-   <input type="number" />
+   <input type="number" value={billInput} onChange={(e) => setBillInput(e.target.value)} />
   </div>;
 };
 
