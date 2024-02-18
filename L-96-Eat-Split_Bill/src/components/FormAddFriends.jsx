23c0454d4352d9af5a0de0ead5aa3a82ff2b00b0
ramanboff <bayramofframan0@gmail.com>
2024-02-18 11:17:44 +0400

    Added FormAddFriends component

---
+++ L-96-Eat-Split_Bill/src/components/FormAddFriends.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from './Button';
+
+const FormAddFriends = () => {
+  return (
+<form className='form-add-friend'>
+  <label>🧑‍🤝‍🧑Friend Name</label>
+  <input type="text" />
+  <label>📷Image URL</label>
+  <input type="text" />
+  <Button>Add</Button>
+</form>
+  )
+};
+
+export default FormAddFriends;
