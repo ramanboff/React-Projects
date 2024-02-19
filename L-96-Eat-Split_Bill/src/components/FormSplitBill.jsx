109c9b0d3ab3f2348293ef000c4b45c6b3b456b1
ramanboff <bayramofframan0@gmail.com>
2024-02-19 17:18:37 +0400

    Builded Static part of application

--- L-96-Eat-Split_Bill/src/components/FormSplitBill.jsx
+++ L-96-Eat-Split_Bill/src/components/FormSplitBill.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button";
 
 const FormSplitBill = () => {
   return (
-    <form className='form-split-bill'>
+    <form className="form-split-bill">
       <h2>Split a bill with X </h2>
+      <label>💰Bill Value</label>
+      <input type="text" />
+      <label>🕴🏽Your expense</label>
+      <input type="text" disabled  />
+      <label>🧑‍🤝‍🧑X's expense</label>
+      <input type="text"/>
+      <label>🤑 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+      <Button>Split Bill</Button>
     </form>
-  )
-}
+  );
+};
 
-export default FormSplitBill
+export default FormSplitBill;
