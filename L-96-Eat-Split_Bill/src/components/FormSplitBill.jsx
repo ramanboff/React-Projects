7556fbbf619bda09c714565fe43ad5d66fc9a75e
ramanboff <bayramofframan0@gmail.com>
2024-02-21 01:25:34 +0400

    value and onChange function to all FormSplitBill inputs

--- L-96-Eat-Split_Bill/src/components/FormSplitBill.jsx
+++ L-96-Eat-Split_Bill/src/components/FormSplitBill.jsx
@@ -15,13 +15,13 @@ const [whoIsPaying, setWhoIsPaying] = useState("user")
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name}</h2>
       <label>💰Bill Value</label>
-      <input type="text" />
+      <input type="text" value={bill} onChange={(e) => setBill(e.target.value)} />
       <label>🕴🏽Your expense</label>
-      <input type="text"  />
+      <input type="text" value={paidByUser} onChange={(e) => setPaidByUser(e.target.value)} />
       <label>🧑‍🤝‍🧑{selectedFriend.name}'s expense</label>
       <input type="text"  disabled/>
       <label>🤑 Who is paying the bill</label>
-      <select>
+      <select value={whoIsPaying} onChange={(e) => setWhoIsPaying(e.target.value)}>
         <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
